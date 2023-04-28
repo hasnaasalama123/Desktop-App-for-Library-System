e9d6adcc91636c47aa9b8361b7729bd2a2d81b82
hasnaasalama123 <122166199+hasnaasalama123@users.noreply.github.com>
2023-04-28 17:38:48 +0200

    Add files via upload

---
+++ book.java
@@ -0,0 +1,8 @@
+public class book {
+    public int Id;
+    public String Name;
+    public String Auther;
+    public String Publisher;
+    public String Category;
+    public String Abstract;
+}
---
+++ student.java
@@ -0,0 +1,9 @@
+
+public class student {
+    public int Id;
+    public String FName;
+    public String LName;
+    public String Address;
+    public String CollegeYear;
+    public String Section; 
+}
